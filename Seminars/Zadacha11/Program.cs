d21abfb6a8a1a6611ac5780d708032f6af646e6e
«Nikolay» <>
2023-02-24 19:58:33 +0300

    zadacha 11

--- Seminars/Zadacha11/Program.cs
+++ Seminars/Zadacha11/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача №11. 
+// Работа в группах Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+//  ● 456 -> 46
+//   ● 782 -> 72
+//   ● 918 -> 98
+
+Console.Clear();
+
+int num = new Random().Next(100,1000);
+Console.WriteLine (num);
+
+int a1 = num / 100;
+int a2 = num % 10;
+
+Console.Write($"{a1}{a2}");
