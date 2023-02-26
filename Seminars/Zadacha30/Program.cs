7b77a261431ff1fa8367fe824f134f31417b98b9
«Nikolay» <>
2023-02-26 16:59:13 +0300

    zadacha 30

---
+++ Seminars/Zadacha30/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//  [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+
+int[] getArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+    return array;
+}
+
+void printArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+
+        Console.Write($" {inArray[i]} ");
+}
+
+int[] array = getArray();
+printArray(array);
