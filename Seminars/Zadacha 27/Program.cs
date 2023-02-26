f0ac2d7a28f7a7ccaa4fa20252f5d72cda35c80d
«Nikolay» <>
2023-02-26 23:55:51 +0300

    zadacha 27-29

---
+++ Seminars/Zadacha 27/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 27: Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11 
+// 82 -> 10 
+// 9012 -> 12
+
+
+// Console.Clear();
+// Console.WriteLine("Enter number: ");
+// int N = int.Parse(Console.ReadLine()!);
+// int K = N % 10;
+// int Sum = K;
+// while (N != 0)
+// {
+// N /= 10;
+// K = N % 10;
+// Sum = Sum + K;
+// }
+// Console.WriteLine($"{Sum}"!);
+
+int Readnumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int a)
+{
+    int sum=0;
+
+    while (a!=0)
+    {
+        sum=sum+a%10;
+        a=a/10;
+    }
+    return sum;
+}
+
+int number = Readnumber("Введите число: ");
+int SumAllNumbers=SumNumbers(number);
+Console.WriteLine(SumAllNumbers);
---
+++ Seminars/Zadacha 29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+// int[] GetArray()
+// {
+// int[] array = new int[8];
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i] = new Random().Next();
+// }
+// return array;
+// }
+// void printArray(int[] NewArray)
+// {
+// for (int i = 0; i < NewArray.Length; i++)
+// Console.Write($"{NewArray[i]} "!);
+// }
+// int[] array = GetArray();
+// printArray(array);
+
+int[] FillRandomArray(int size)
+{
+    int [] array=new int[size];
+    for(int i=0; i < array.Length; i++)
+    {
+        array[i]=new Random().Next(0,9);
+    }
+    return array;
+}
+
+int [] array1=FillRandomArray(8);
+Console.WriteLine(string.Join(", ", array1));
