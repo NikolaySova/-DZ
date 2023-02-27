1597f45caed363de3baa5f626c731160238fb9fc
«Nikolay» <>
2023-02-27 18:26:23 +0300

    zadacha 341

---
+++ Seminars/Zadacha 41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+
+// 1, -7, 567, 89, 223-> 4
+
+ int number;
+ int count=0;
+string stopWord ="stop";
+
+ while(true)
+ {
+    string answer=Console.ReadLine();
+    if(answer==stopWord)
+    {
+        break;
+    }
+    number=Convert.ToInt32(answer);
+    if( number>0) count++;
+ }
+
+Console.WriteLine(count);
